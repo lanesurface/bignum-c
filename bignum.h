8d39a569a3fd3d0558f1c12025ce8ea138dff33a
Lane W. Surface <surfacelane@gmail.com>
2019-06-20 19:28:44 -0500

    Remove build files

--- bignum.h
+++ bignum.h
@@ -13,23 +13,8 @@
  * limitations under the License.
  */
 
-/*
- * File: bignum.h
- * Authors: Lane W. Surface
- * Version: 1.0
- *
- * This file contains functions for manipulating and working with arbitrarily
- * sized numbers. Not only does this abstract away from the underlying
- * bit-patterns, placing focus only on the presentable representation
- * thereof; but allows for numbers which cannot be represented in the native
- * C integer types provided with the language.
- */
-
 #ifndef __BIGNUM_H__
 #define __BIGNUM_H__
-
-#include <math.h>
-#include <string.h>
 
 typedef char *bignum_t;
 
